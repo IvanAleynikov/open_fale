5d8dfe601c93777c682e18ef38cb4d80184747b9
Ivan Aleinikov <alejnikov99.99@mail.ru>
2023-12-10 14:52:58 +0300

    cook_book

---
+++ main.py
@@ -0,0 +1,32 @@
+# Задание 1
+def get_cook_book():
+    with open('recipes.txt', encoding='utf-8') as f:
+        cook_book = {}
+        for line in f:
+            dish_name = line.strip()
+            ingredients_list = []
+            ingredients_quantity = f.readline()
+            for _ in range(int(ingredients_quantity)):
+                ingredient_name, quantity, measure = f.readline().split(' | ')
+                ingredients_list.append({'ingredient_name': ingredient_name, 'quantity': int(quantity), 'measure': measure.strip()})
+            cook_book[dish_name] = ingredients_list
+            f.readline()
+    return cook_book
+
+# Задание 2
+
+def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = get_cook_book()
+    shop_dict = {}
+    for dish in dishes:
+        if dish in cook_book.keys():
+            for recipe in cook_book[dish]:
+                if recipe['ingredient_name'] in shop_dict:
+                    shop_dict[recipe['ingredient_name']]['quantity'] += recipe['quantity'] * person_count
+                else:
+                    shop_dict[recipe['ingredient_name']] = {'measure': recipe['measure'], 'quantity': (recipe['quantity'] * person_count)}
+        else:
+            error = 'Ошибка: введённого Вами блюда нет в кулинарной книге'
+            return error
+    return shop_dict
+
